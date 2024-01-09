8a8ebb558dd3d330f59ff633ccd50540838d24d6
WoojinJeonkr <wooeric1@naver.com>
2024-01-09 23:49:00 +0900

    fix: modify SITE_TITLE

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Woojin's Blog";
+export const SITE_TITLE = "Tech Chronicles";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
+  "Welcome to my blog!";
 export const TWITTER_HANDLE = "@yourtwitterhandle";
 export const MY_NAME = "Jeon Woojin";
 
